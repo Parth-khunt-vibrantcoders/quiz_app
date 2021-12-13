b0bd701f1881cd6b3c03791644269e06c32139d1
Parth <pkhunt@vibrantcoders.com>
2021-12-13 22:13:45 +0530

    change in counting

--- resources/views/frontend/pages/register/register.blade.php
+++ resources/views/frontend/pages/register/register.blade.php
@@ -28,11 +28,44 @@
                 </div>
             </div>
             <ul>
-                <li><a href="">Menu 1</a></li>
-                <li><a href="">Menu 2</a></li>
-                <li><a href="">Menu 3</a></li>
-                <li><a href="">Menu 4</a></li>
+                <li>
+                    <a href="{{ route('quiz-list')}}">
+                        <i class="fa fa-home" aria-hidden="true"></i>Home
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="fa fa-handshake" aria-hidden="true"></i>Partner with us
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="fa fa-envelope" aria-hidden="true"></i>Contact us
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('quiz-rules') }}">
+                        <i class="fa fa-flag" aria-hidden="true"></i>Qzop Rules
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('privacy-policy') }}">
+                        <i class="fa fa-lock" aria-hidden="true"></i>Privacy Policy
+                    </a>
+                </li>
             </ul>
+            <div class="submit-btn">
+                <p>Join qzop to play quiz and earn coins</p>
+                @if (!empty($data))
+                    <a href="{{ route('logout', ['id' => $_GET['id']]) }}">
+                        <button type="submit" class="btn">Logout</button>
+                    </a>
+                @else
+                    <a href="{{ route('sign-in', ['id' => $_GET['id']]) }}">
+                        <button type="submit" class="btn">Join Now</button>
+                    </a>
+                @endif
+            </div>
             <div class="submit-btn">
                 <p>Join qzop to play quiz and earn coins</p>
                 <button type="submit" class="btn">Join Now</button>
