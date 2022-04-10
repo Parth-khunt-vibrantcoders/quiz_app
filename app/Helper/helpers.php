cc7d5fccb4be687432f4d65538d3b103f8d2b6d5
Parth <pkhunt@vibrantcoders.com>
2022-04-11 00:02:45 +0530

    change incountry

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -112,7 +112,7 @@ function get_users_coin($userid){
         );
         if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
             $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
-            ccd($ipdat->geoplugin_countryName);
+
             return $ipdat->geoplugin_countryName;
         }
         return false;
